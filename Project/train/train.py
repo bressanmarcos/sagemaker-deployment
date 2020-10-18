d5a04847134d32429dd1edaa558d443b25588ffd
bressan@dee.ufc.br <bressan@dee.ufc.br>
2020-10-18 03:02:53 +0000

    Completion of exercises

--- Project/train/train.py
+++ Project/train/train.py
@@ -56,20 +56,36 @@ def _get_train_data_loader(batch_size, training_dir):
 
 
 def train(model, train_loader, epochs, optimizer, loss_fn, device):
-    """
-    This is the training method that is called by the PyTorch training script. The parameters
-    passed are as follows:
-    model        - The PyTorch model that we wish to train.
-    train_loader - The PyTorch DataLoader that should be used during training.
-    epochs       - The total number of epochs to train for.
-    optimizer    - The optimizer to use during training.
-    loss_fn      - The loss function used for training.
-    device       - Where the model and data should be loaded (gpu or cpu).
-    """
-    
-    # TODO: Paste the train() method developed in the notebook here.
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
 
-    pass
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+
+            # TODO: Complete this train method to train the model provided.
+
+            # Reset gradients
+            optimizer.zero_grad()
+
+            # Forward
+            output = model(batch_X)
+
+            # Loss
+            loss = loss_fn(output, batch_y)
+
+            # Backward propagation
+            loss.backward()
+
+
+            # Call step of optimizer to update model params
+            optimizer.step()
+
+            total_loss += loss.data.item()
+
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 
 if __name__ == '__main__':
